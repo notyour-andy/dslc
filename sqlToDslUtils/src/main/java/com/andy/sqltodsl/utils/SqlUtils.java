bd678c45c00d44fcee2d113d1045edd3359c4ead
Andy <2365016350@qq.com>
2022-11-18 00:50:58 +0800

    补充

--- sqlToDslUtils/src/main/java/com/andy/sqltodsl/utils/SqlUtils.java
+++ sqlToDslUtils/src/main/java/com/andy/sqltodsl/utils/SqlUtils.java
@@ -4,33 +4,12 @@ import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
 import com.alibaba.druid.stat.TableStat;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.google.inject.internal.util.ImmutableMap;
 import com.google.inject.internal.util.Preconditions;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.*;
 
 public class SqlUtils {
-
-
-    private static JSONObject TYPETEXT = JSONObject.parseObject("{\"type\":\"text\",\"fielddata\":true}");
-
-
-    private final static Map<String,Object> SORT = ImmutableMap.of("type","keyword");
-
-    private final static Map<String,Object> fields = ImmutableMap.of("sort",SORT);
-
-//    private final static Map<String,Object> TYPEMAPWITHORDER = ImmutableMap.of("type","text","fields",fields,"fielddata",true);
-
-    //    private final static Map<String,String> TYPELONGMAP = ImmutableMap.of("type","long");
-    private static Map<String,String> TYPELONGMAP =
-            new HashMap<String, String>() {
-                {
-                    put("type", "long");
-                }
-            };
 
     private static final String FIELD = "field";
 
@@ -39,14 +18,6 @@ public class SqlUtils {
     private static final String OPERATOR = "operator";
 
     public static void main(String[] args) {
-        JSONArray jsonArray1 = JSONArray.parseArray("[{\"name\":\"标题\",\"key\":\"G_title\",\"type\":\"text\"},{\"name\":\"是否提交\",\"key\":\"G_isSubmit\",\"type\":\"text\"},{\"name\":\"基本信息_名字\",\"type\":\"text\",\"key\":\"P_getBaseInfo_name\"},{\"name\":\"基本信息_年龄\",\"type\":\"text\",\"key\":\"P_getBaseInfo_age\"},{\"name\":\"基本信息_出生年份\",\"type\":\"number\",\"key\":\"P_getBaseInfo_year\"},{\"name\":\"基本信息_多选\",\"key\":\"P_getBaseInfo_duoxuan\",\"type\":\"text\",\"children\":[{\"name\":\"值\",\"key\":\"value\",\"type\":\"text\"},{\"name\":\"标签\",\"key\":\"label\",\"type\":\"text\"}]},{\"name\":\"基本信息_范围\",\"type\":\"text\",\"key\":\"P_getBaseInfo_fanwei\"},{\"name\":\"基本信息_序号\",\"type\":\"text\",\"key\":\"P_getBaseInfo_xuhao\"},{\"name\":\"基本信息_上传pdf\",\"key\":\"P_getBaseInfo_pdf\",\"type\":\"text\",\"children\":[{\"name\":\"文件id\",\"key\":\"id\",\"type\":\"text\"},{\"name\":\"文件名称\",\"key\":\"name\",\"type\":\"text\"}]}]");
-        String sql = "select * from z_test where a = '1' and b.d = '2' or (c = '3' or ( d = '4' and e = '5'))";
-        List<String> dataSet = parseQueryConditions(sql);
-//        System.out.println("dd");
-//        String whereStatement = getWhereStatement(sql);
-        System.out.println(dataSet);
-        char c = '1';
-        System.out.println((int) c);
     }
 
 
@@ -108,8 +79,8 @@ public class SqlUtils {
         MySqlSchemaStatVisitor visitor = getVisitor(sql);
         List<String> tableNameList = getTableNameList(visitor);
         for (TableStat.Condition con : visitor.getConditions()) {
-            dataMap = new HashMap<>();
             Class<?> aClass = con.getValues().get(0).getClass();
+            dataMap = new HashMap<>();
             dataMap.put(FIELD, getFieldName(tableNameList, con));
             dataMap.put(VALUE, Objects.equals(aClass.getName(), "java.lang.String") ? "'" + con.getValues().get(0) + "'" : con.getValues().get(0));
             dataMap.put(OPERATOR, con.getOperator());
@@ -147,53 +118,5 @@ public class SqlUtils {
     public static String getWhereStatement(String sql){
         int index = sql.toUpperCase(Locale.ROOT).indexOf("WHERE");
         return sql.substring(index + "WHERE".length());
-    }
-
-    public static Map<String, Object> initMapNew(JSONArray array, Map<String,Object> propertiesMap){
-        for (int i = 0; i < array.size(); i++) {
-            JSONObject jsonObject = array.getJSONObject(i);
-            String key = jsonObject.getString("key");
-            if (!jsonObject.containsKey("children")){
-                //如果不含有children属性, 则为text类型
-                String typeName = jsonObject.getString("type");
-                if(Objects.equals(typeName,"text")){
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("type", "text");
-                    map.put("fielddata", true);
-//                    BeanUtils.copyProperties(TYPETEXT, map);
-//                    System.out.println(TYPETEXT);
-//                    System.out.println(map);
-                    propertiesMap.put(key, TYPETEXT);
-                }else {
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("type", "long");
-                    propertiesMap.put(key, TYPELONGMAP);
-                }
-            }else{
-                JSONArray children = jsonObject.getJSONArray("children");
-//                JSONArray newChildren = JSONArray.parseArray(JSONObject.toJSONString(jsonObject.getJSONArray("children")));
-                String typeName = jsonObject.getString("type");
-                if (CollectionUtils.isNotEmpty(children)){
-                    JSONObject json = new JSONObject();
-                    json.put("type", "nested");
-                    json.put("properties", initMapNew(children, new HashMap<>()));
-                    propertiesMap.put(key, json);
-                }else{
-                    if(Objects.equals(typeName,"text")){
-                        Map<String, Object> map = new HashMap<>();
-                        map.put("type", "text");
-                        map.put("fielddata", true);
-                        propertiesMap.put(key, map);
-                    }else {
-                        Map<String, Object> map = new HashMap<>();
-                        map.put("type", "long");
-                        propertiesMap.put(key, map);
-                    }
-                }
-
-
-            }
-        }
-        return propertiesMap;
     }
 }
